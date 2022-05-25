4cdc467acf86ee37339141cf17822e8773aa8f3e
vitor <vitor.petro2@gmail.com>
2022-05-25 10:13:01 -0300

    Criação do projeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projFinal.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Serie meuObjeto = new Serie();
+
+        
+            Console.WriteLine("Hello world");
+        }
+    }
+}
